fd8c07c25c146b578dbfef677a86e99b9282889c
[froebbm] <[brian.froeb@gmail.com]>
2024-01-15 23:53:07 -0500

    adding rproj to gitignore

---
+++ code/collection/creating_barcelona_boundary.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import geopandas as gpd
+
+from pprint import pprint
+
+cities = pd.read_csv(
+    "D:\\Documents\\network_ratios\\data\\barcelona\\barcelona_cities.csv"
+    )
+
+print(cities)
+
---
+++ code/collection/data_pull.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import geopandas as gpd
+
+from sam.replica import Replica
+from rputils import make_path
+
+atlanta = Replica(
+    
+)
